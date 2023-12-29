a11d619377ad646985e0782ad9dd82f02139e2bd
hudaiquan <hudaiquan@pcitech.com>
2023-12-29 18:18:29 +0800

    feat():pom 添加validator

--- hucloud-webs/src/main/java/com/expand/hucloud/webs/HucloudWebsApplication.java
+++ hucloud-webs/src/main/java/com/expand/hucloud/webs/HucloudWebsApplication.java
@@ -1,10 +1,17 @@
 package com.expand.hucloud.webs;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.hibernate.validator.HibernateValidator;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 @SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class,scanBasePackages = {"com.expand.hucloud"})
 @EnableAsync
@@ -13,6 +20,25 @@ public class HucloudWebsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HucloudWebsApplication.class, args);
+    }
+
+    @Bean
+    public Validator validator() {
+        // 配置为快速返回失败模式
+        ValidatorFactory validatorFactory = Validation.byProvider(HibernateValidator.class)
+                .configure()
+                .addProperty("hibernate.validator.fail_fast", "true")
+                .buildValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+        return validator;
+    }
+
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        MethodValidationPostProcessor postProcessor = new MethodValidationPostProcessor();
+        // 设置validator模式为快速失败返回
+        postProcessor.setValidator(validator());
+        return postProcessor;
     }
 
 }
