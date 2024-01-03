c5f3bcb207021495bac97ea145033fea7a6c91dc
hudaiquan <hudaiquan@pcitech.com>
2024-01-03 17:54:26 +0800

    feat():ip依赖

---
+++ hucloud-common/src/main/java/com/expand/hucloud/common/utils/IpRegionUtil.java
@@ -0,0 +1,22 @@
+package com.expand.hucloud.common.utils;
+
+import java.io.File;
+
+import org.apache.commons.lang3.StringUtils;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ *
+ * 获取ip城市
+ * @author hdq
+ * @time 2024/1/3 17:36
+ */
+public class IpRegionUtil {
+
+
+
+
+}
